3db0347db26b18a7ba2674b8c2135725b2b6400d
rishrc <96913702+rishrc@users.noreply.github.com>
2022-01-02 12:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from proj import finalfunc
 from exp import explore
 ide_ra,hush,res=0,0,0
 st.set_page_config(layout="wide")
-st.title("Products Comparison")
+st.title("REAL TIME PRODUCT ANALYSIS USING WEB SCRAPING")
 i=st.text_input(label='SEARCH HERE...')
 if i:
     
-    st.write(i+"Product searching")
+    st.write("Searching for "+i)
     ide_ra,hush,res=finalfunc(i)
 else:
     if ide_ra!=0:
         st.write(ide_ra[0][0])
     else:
-        st.write("no input")
+        st.write("")
 explore(ide_ra,hush)
 

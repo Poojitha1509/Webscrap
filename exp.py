5f28fa6e930a09c1cca025c1cb135872075d7c05
rishrc <96913702+rishrc@users.noreply.github.com>
2022-01-02 13:17:09 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -11,9 +11,9 @@ def explore(ide_ra,hush):
             for i in range(5):
                 st.subheader(ide_ra[i][0])
                 st.markdown("<style>.big-font {color:red;font-size:25px;} .d{font-size:20px;}</style>", unsafe_allow_html=True)
-                st.markdown("<p class=\"big-font\">Price "+ide_ra[i][1]+"</p>", unsafe_allow_html=True)
-                st.markdown("<p class=\"d\">Rating "+ide_ra[i][2]+"</p>", unsafe_allow_html=True)
-                st.markdown("<p class=\"d\">Review Count "+ide_ra[i][3]+"</p>", unsafe_allow_html=True)
+                st.markdown("<p class=\"big-font\">Price - "+ide_ra[i][1]+"</p>", unsafe_allow_html=True)
+                st.markdown("<p class=\"d\">Rating - "+ide_ra[i][2]+"</p>", unsafe_allow_html=True)
+                st.markdown("<p class=\"d\">Reviews Count - "+ide_ra[i][3]+" Ratings</p>", unsafe_allow_html=True)
                 st.write("Click this [link]("+ide_ra[i][4]+") to visit product page.")
                                 
 
@@ -25,9 +25,9 @@ def explore(ide_ra,hush):
                 if True:
                     st.subheader(ide_ra[i][0])
                     st.markdown("<style>.big-font {color:red;font-size:25px;} .d{font-size:20px;}</style>", unsafe_allow_html=True)
-                    st.markdown("<p class=\"big-font\">Price "+hush[i][0]+"</p>", unsafe_allow_html=True)
-                    st.markdown("<p class=\"d\">Rating "+hush[i][1]+"out of 5 stars</p>", unsafe_allow_html=True)
-                    st.markdown("<p class=\"d\">Review Count "+hush[i][2]+"</p>", unsafe_allow_html=True)
+                    st.markdown("<p class=\"big-font\">Price - "+hush[i][0]+"</p>", unsafe_allow_html=True)
+                    st.markdown("<p class=\"d\">Rating - "+hush[i][1]+" out of 5 stars</p>", unsafe_allow_html=True)
+                    st.markdown("<p class=\"d\">Reviews Count - "+hush[i][2]+"</p>", unsafe_allow_html=True)
                     st.write("Click this [link]("+hush[i][3]+") to visit product page.")
     else:
         st.write("")
